122885d73a0e5468c28cdc42bd136fb0790c166f
Rui Guo <guojiarui@gmail.com>
2022-07-03 22:00:10 -0700

    challenge 12 mark as completed.

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -19,7 +19,7 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  // Solution code here...
+  return /^\d{4}$/.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -31,7 +31,7 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 ------------------------------------------------------------------------------------------------ */
 
 const validateWord = (word) => {
-  return word.matches('(?=(.*?\\d[a-zA-Z]{5,10}');
+  return /^[a-zA-Z]{5,10}$/.test(word);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -43,7 +43,7 @@ If it does, return true. If not, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const hasNumber = (string) => {
-  // Solution code here...
+  return /[a-zA-Z]+\d+/.test(string);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,7 +63,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  return /^[a-zA-Z0-9]+\.?[a-zA-Z0-9]+@[a-zA-Z0-9]+\.(com|net|org)$/.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -88,9 +88,8 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+  return /^(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/.test(phoneNumber);
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
 
