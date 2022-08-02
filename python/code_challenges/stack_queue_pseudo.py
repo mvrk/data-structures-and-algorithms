aaeb9c0c4c18be2180c1bc19cf71c0c9f9937d84
Alex Guo <guojiarui@gmail.com>
2022-08-01 23:54:00 -0700

    stack_queue_pseudo all tests passed.

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,17 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.en_stack = Stack()
+        self.de_stack = Stack()
+
+    def enqueue(self, elem):
+        self.en_stack.push(elem)
+
+    def dequeue(self):  # pop out the last item using FIFO rule
+        if self.de_stack.is_empty():
+            while not self.en_stack.is_empty():
+                self.de_stack.push(self.en_stack.pop())
+        return self.de_stack.pop()
+
+
