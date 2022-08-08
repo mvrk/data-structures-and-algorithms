beb2c65489cee9414a0877b7e52517d0c92ddae4
Alex Guo <guojiarui@gmail.com>
2022-08-08 15:25:04 -0700

    code passed all tests.

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,22 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    queue = Queue()
+    node = []
+
+    if not tree:  # empty tree
+        return node
+    if not tree.root:  # tree only have one node
+        return node
+    if not queue.front:  # queue is empty
+        queue.enqueue(tree.root)
+    while queue.front:
+        root = queue.dequeue()
+        node.append(root.value)
+        if root.left:
+            queue.enqueue(root.left)
+        if root.right:
+            queue.enqueue(root.right)
+    return node
