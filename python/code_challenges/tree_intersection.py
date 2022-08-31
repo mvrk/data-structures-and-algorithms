de9612b5663b2bc72c696d101c7f35c1cc53a449
Alex Guo <guojiarui@gmail.com>
2022-08-30 23:09:03 -0700

    code pass all tests

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,21 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
+import pytest
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree_a, tree_b):
+    match = set()
+    hashtable = Hashtable()
+    tree_a_value = BinaryTree.pre_order(tree_a)
+    tree_b_value = BinaryTree.pre_order(tree_b)
+
+    for num in tree_a_value:
+        hashtable.set(num, num)
+
+    for num in tree_b_value:
+        if hashtable.contains(num):
+            match.add(num)
+
+    return match
+
+
