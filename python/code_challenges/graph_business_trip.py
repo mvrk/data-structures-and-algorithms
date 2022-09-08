1a10b04326e2e34877267c53ceda4f075a6981e3
Alex Guo <guojiarui@gmail.com>
2022-09-07 20:00:57 -0700

    can not figure out the KeyError during testing, will ask for help tomorrow.

--- python/code_challenges/graph_business_trip.py
+++ python/code_challenges/graph_business_trip.py
@@ -1,5 +1,26 @@
 from data_structures.graph import Graph
 
 
-def direct_flights():
-    pass
+def direct_flights(graph, city):
+    if len(city) == 0:
+        return
+    nodes = {}
+    for vertex in graph.get_nodes():
+        nodes[vertex.value] = vertex
+
+    cost = 0
+    flag = False
+
+    for cn in range(len(city) - 1):
+        edge = graph.get_neighbors(city[cn])
+        for e in edge:
+            if city[cn + 1] == e.vertex:
+                cost += e.weight
+                flag = True
+
+    if not flag:
+        cost = 0
+        flag = False
+        return f'{flag},${cost}'
+
+    return f'{flag},${cost}'
